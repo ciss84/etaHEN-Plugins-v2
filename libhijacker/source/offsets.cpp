4f9fd004da6c65616321ef6d896ef296a08fed92
Mugi <37186888+ciss84@users.noreply.github.com>
2026-02-23 13:19:09 +0100

    Update offsets.cpp

--- libhijacker/source/offsets.cpp
+++ libhijacker/source/offsets.cpp
@@ -176,13 +176,13 @@ namespace offsets {
             case V700: case V701: case V720: case V740: case V760: case V761:
                 return 0x0AC8064 + 0x24;
             case V800: case V820: case V840: case V860:
-                return 0x0AC3064 + 0x24;
+                return 0x0AC3088; //0x0AC3064 + 0x24
             case V900:
-                return 0x0D72064 + 0x24;
+                return 0x0D72088; //0x0D72064 + 0x24
             case V905: case V920: case V940: case V960:
-                return 0x0D73064 + 0x24;
+                return 0x0D73088; //0x0D73064 + 0x24
             case V1000: case V1001: case V1020: case V1040: case V1060:
-                return 0x0D79064 + 0x24;
+                return 0x0D79088; //0x0D79064 + 0x24
             default:
                 printf("Unsupported firmware version: 0x%x\n", getSystemSwVersion() & VERSION_MASK);
                 return -1;
@@ -206,13 +206,13 @@ namespace offsets {
             case V700: case V701: case V720: case V740: case V760: case V761:
                 return 0x0AC8064 + 0x8C;
             case V800: case V820: case V840: case V860:
-                return 0x0AC3064 + 0x8C;
+                return 0x0AC30F0; //0x0AC3064 + 0x8C
             case V900:
-                return 0x0D72088; //0x0D72064 + 0x8C
+                return 0x0D720F0; //0x0D72064 + 0x8C
             case V905: case V920: case V940: case V960:
-                return 0x0D73088; //0x0D73064 + 0x8C
+                return 0x0D730F0; //0x0D73064 + 0x8C
             case V1000: case V1001: case V1020: case V1040: case V1060:
-                return 0x0D79088; //0x0D79064 + 0x8C
+                return 0x0D790F0; //0x0D79064 + 0x8C
             default:
                 printf("Unsupported firmware version: 0x%x\n", getSystemSwVersion() & VERSION_MASK);
                 return -1;
